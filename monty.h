47bdd5f16f3da45eef96057cca840b379bb40155
ThatsVie <Viepaulalmt@gmail.com>
2023-12-10 13:59:56 -0800

    added prototype for execute

--- monty.h
+++ monty.h
@@ -43,4 +43,5 @@ void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
+void execute_opcode(char *opcode, stack_t **stack, unsigned int line_number);
 #endif

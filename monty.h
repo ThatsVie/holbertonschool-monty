560523d990e7538a48f66b4b90225c3f6a713fc9
ThatsVie <Viepaulalmt@gmail.com>
2023-12-10 12:53:29 -0800

    updating header with libraries and prototypes

--- monty.h
+++ monty.h
@@ -1,6 +1,12 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -29,3 +35,10 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instructions_t;
+
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **current_stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+
+#endif

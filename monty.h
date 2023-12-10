db2ff09a7456f2d08cbcb6bdb25e5c454aec0ebd
ThatsVie <Viepaulalmt@gmail.com>
2023-12-10 14:46:59 -0800

    added main.c prototype

--- monty.h
+++ monty.h
@@ -45,4 +45,5 @@ void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void execute_opcode(char *opcode, stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *stack);
+int main(int argc, char **argv);
 #endif

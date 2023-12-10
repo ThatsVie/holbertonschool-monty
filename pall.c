68f0fae6e7db684f848b949beb86a97c83f097b8
ThatsVie <Viepaulalmt@gmail.com>
2023-12-10 14:08:13 -0800

    commenting

--- pall.c
+++ pall.c
@@ -3,11 +3,14 @@
  * pall - prints all elements of the stack
  * @stack: pointer to the stack (linked list)
  * @line_number : line number in the Monty bytecode file
+ * Return: void
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
+	/* Unused parameter, mentioned to avoid compiler warnings. */
 	(void)line_number;
 
+	/* Traverse the stack and print each element. */
 	while (*stack)
 	{
 		printf("%i\n", (*stack)->n);
